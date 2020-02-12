bc69bb724052561c57e4648c96325db60d905106
Jessie Cruz <jac237@ucsd.edu>
2020-02-11 21:30:49 -0800

    Added index route

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
 
+var app = express();
+
+// Example route
+// var user = require('./routes/user');
+var index = require('./routes/index');
 var home = require('./routes/home');
 var calendar = require('./routes/calendar');
 var rules = require('./routes/rules');
@@ -14,10 +19,6 @@ var finance = require('./routes/finance');
 var settings= require('./routes/settings');
 var chores = require('./routes/chores');
 var appliance = require('./routes/appliance');
-// Example route
-// var user = require('./routes/user');
-
-var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -39,17 +40,19 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//app.get('/hello/:userName', hello.view);
+// Example route
+// app.get('/users', user.list);
+
 // Add routes here
-app.get('/', home.view);
+app.get('/', index.view);
+app.get('/home', home.view);
 app.get('/calendar', calendar.view);
 app.get('/finance', finance.view);
 app.get('/rules', rules.view);
 app.get('/settings', settings.view);
 app.get('/chores', chores.view);
 app.get('/appliance', appliance.view);
-//app.get('/hello/:userName', hello.view);
-// Example route
-// app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
