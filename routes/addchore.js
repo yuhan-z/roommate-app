ea0f070f027978d4f8cfd6aa6996cc4c536cfe91
Yuhan Zhang <yuz053@ucsd.edu>
2020-02-14 07:48:27 -0800

    calendar

---
+++ routes/addchore.js
@@ -0,0 +1,14 @@
+var data = require("../data.json");
+
+exports.addChore = function(request, response) {    
+	// Your code goes here
+	var newEvent = {
+		"color": request.query.color,
+		"description": request.query.description,
+		"date": request.query.date			
+	};
+	console.log(newEvent);
+	data.calendar.push(newEvent);
+	response.render('calendar',data);
+
+ }
--- routes/calendar.js
+++ routes/calendar.js
@@ -1,7 +1,8 @@
 /*
  * GET calendar page.
  */
+var data = require('../data.json');
 
 exports.view = function (req, res) {
-  res.render('calendar');
+  res.render('calendar',data);
 };
