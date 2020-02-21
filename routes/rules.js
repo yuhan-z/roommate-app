b58f8c3d787761c83633a26f47ffb34b2bc2fb94
Yuhan Zhang <yuz053@ucsd.edu>
2020-02-20 21:24:50 -0800

    edit rules

--- routes/rules.js
+++ routes/rules.js
@@ -7,3 +7,17 @@ var data = require('../data.json');
 exports.view = function (req, res) {
   res.render('rules', data);
 };
+
+exports.add = function (req, res) {
+	var newRule = {
+		"description": req.query.rule,		
+	};
+	data.rules.push(newRule);
+	res.render('rules', data);
+}
+
+exports.remove = function (req, res){
+	index = req.query.index;
+	data.rules.splice(index,1);
+	res.render('rules', data);
+}
