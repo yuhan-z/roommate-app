ae653d8f410a84a6f0038ed5217469a289a7d552
Jessie Cruz <jac237@ucsd.edu>
2020-02-28 02:43:54 -0800

    fixed null user case

--- routes/home.js
+++ routes/home.js
@@ -5,13 +5,21 @@ var data = require('../data.json');
  */
 
 exports.view = function (req, res) {
-	name = req.query.name;
-	if(name != null) data["name"] = name.split("@")[0];
+    name = req.query.name;
+    if (name != null) {
+        data["name"] = name.split("@")[0];
+    } else {
+        data["name"] = "User";
+    }
     res.render('home', data);
 };
 
 exports.viewAlt = function (req, res) {
-	name = req.query.name;
-	if(name != null) data["name"] = name.split("@")[0];
+    name = req.query.name;
+    if (name != null) {
+        data["name"] = name.split("@")[0];
+    } else {
+        data["name"] = "User";
+    }
     res.render('homeAlt', data);
 };
