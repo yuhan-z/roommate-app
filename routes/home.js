b2cf4965bac845e8c21bdc7966936e9790bde11a
Yuhan Zhang <yuz053@ucsd.edu>
2020-02-27 23:11:41 -0800

    removed homeAlt

--- routes/home.js
+++ routes/home.js
@@ -7,11 +7,13 @@ var data = require('../data.json');
 exports.view = function (req, res) {
 	name = req.query.name;
 	if(name != null) data["name"] = name.split("@")[0];
+	data["viewAlt"] = false;
     res.render('home', data);
 };
 
 exports.viewAlt = function (req, res) {
 	name = req.query.name;
 	if(name != null) data["name"] = name.split("@")[0];
-    res.render('homeAlt', data);
+	data["viewAlt"] = true;
+    res.render('home', data);
 };
