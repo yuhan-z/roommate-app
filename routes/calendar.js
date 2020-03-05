ad8ccd1ba18460d5de61317b31c34cf45434aa95
Jessie Cruz <jac237@ucsd.edu>
2020-03-05 04:32:08 -0800

    fixed chore assignment with 'get' method

--- routes/calendar.js
+++ routes/calendar.js
@@ -10,10 +10,12 @@ exports.view = function (req, res) {
 exports.reserveApp = function(request, response) {
     var roommate = data.housemates[request.query.roommate];
     var appliance = data.appliances[request.query.appliance];
+    var date = request.query.date;
+    var time = request.query.time
 	var newEvent = {
 		"name": roommate,
-		"description": appliance + ' ' + request.query.time,
-		"date": request.query.date,
+		"description": appliance + ' ' + time,
+		"date": date,
 		"category": 'reservation'	
 	};
 	console.log(newEvent);
@@ -23,12 +25,13 @@ exports.reserveApp = function(request, response) {
  
 exports.assignChore = function(request, response) {
     var roommate = data.housemates[request.query.roommate];
-    var appliance = data.appliances[request.query.appliance];
+    var chore = data.chore_list[request.query.chore];
+    var date = request.query.date;
 	var newEvent = {
 		"name": roommate,
-		"description": appliance + ' ' + request.query.time,
-		"date": request.query.date,
-		"category": 'reservation'	
+		"description": chore,
+		"date": date,
+		"category": 'chore'
 	};
 	console.log(newEvent);
 	data.calendar.push(newEvent);
